ee49ce2eee72bceaabe887ab72db10b929911082
Heart <supalit1@gmail.com>
2021-07-29 14:20:13 +0700

    up

--- script.js
+++ script.js
@@ -5,9 +5,27 @@ const length = document.getElementById('length')
 const color = document.querySelector('#color')
 
 // define more constants and variables here
+let leninput = ''
+const input = document.getElementById('length')
+input.addEventListener('length', ev => {
+    leninput = ev.target.value
 
+})
+let coinput = ''
+const input = document.getElementById('color')
+input.addEventListener('color', ev => {
+    coinput = ev.target.value
+
+})
 btn_toggle.onclick = () => {
   // your code here
 }
 
 // more codes for Search and Reset buttons here
+const search = () => {
+  
+}
+  
+
+ 
+})
